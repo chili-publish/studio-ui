7e6325b421910457fd014c73eb2405a2b925e877
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2023-04-01 15:06:55 +0300

    add and configure jest

---
+++ src/tests/dum.test.ts
@@ -0,0 +1,8 @@
+// TODO: remove me when you create legitimate tests
+describe('Dum test', () => {
+    it('just a placeholder test', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
+
+export {};
