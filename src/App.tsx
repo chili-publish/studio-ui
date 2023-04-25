7811900af1c482dcf96e6cb20708be145477bbd8
Abdelhalim Khouas <42040066+abdelhalimkhouas@users.noreply.github.com>
2023-04-25 21:08:26 +0300

    [Feature] Add workspace loader to grafx (EUW part) (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
 
-function App() {
+interface projectConfig {
+    templateDownloadUrl: string;
+    templateUploadUrl: string;
+    templateId: string;
+    graFxStudioEnvironmentApiBaseUrl: string;
+    authToken?: string;
+}
+function App({ projectConfig, editorLink }: { projectConfig?: projectConfig; editorLink: string }) {
     return (
         <div className="App">
             <h1>End User workspace</h1>
+            <h3>editorLink : {editorLink}</h3>
+            <h3>authtoken : {projectConfig?.authToken}</h3>
+            <h3>templateDownloadUrl : {projectConfig?.templateDownloadUrl}</h3>
+            <h3>templateUploadUrl : {projectConfig?.templateUploadUrl}</h3>
+            <h3>templateId : {projectConfig?.templateId}</h3>
+            <h3>graFxStudioEnvironmentApiBaseUrl : {projectConfig?.graFxStudioEnvironmentApiBaseUrl}</h3>
         </div>
     );
 }
