440a1d245f501242dba6c7782fbb3550e79d0361
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2023-05-02 19:07:34 +0300

    fix conflict and styling

--- src/App.tsx
+++ src/App.tsx
@@ -123,18 +123,11 @@ function App({ projectConfig, editorLink }: { projectConfig?: projectConfig; edi
     });
 
     return (
-        <div>
+        <div style={{ height: '100vh' }}>
             <Navbar />
             <div className="editor-workspace-canvas" data-id="layout-canvas">
                 <div id="chili-editor" style={{ width: '100%', height: '100%' }} />
             </div>
-            <h1>End User workspace</h1>
-            <h3>editorLink : {editorLink}</h3>
-            <h3>authtoken : {projectConfig?.authToken}</h3>
-            <h3>templateDownloadUrl : {projectConfig?.templateDownloadUrl}</h3>
-            <h3>templateUploadUrl : {projectConfig?.templateUploadUrl}</h3>
-            <h3>templateId : {projectConfig?.templateId}</h3>
-            <h3>graFxStudioEnvironmentApiBaseUrl : {projectConfig?.graFxStudioEnvironmentApiBaseUrl}</h3>
         </div>
     );
 }
