413e982f4d4e7695accd853927578534a4e5b856
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2023-05-09 20:31:29 +0300

    Add hook to handle actions triggered by variable component

---
+++ src/components/variableComponent/useVariableComponent.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import { Id, ImageVariableSource } from '@chili-publish/editor-sdk';
+
+export const useVariableComponents = (currentVariable: Id) => {
+    const closePanel = () => {
+        console.log('%c⧭', 'color: #ff0000', 'Closing the panel');
+    };
+
+    const handleImageChange = (source: ImageVariableSource) => {
+        console.log('%c⧭', 'color: #00e600', 'changing the image to', source);
+    };
+
+    const handleImageRemove = () => {
+        console.log('%c⧭', 'color: #00a3cc', 'removing the image');
+    };
+
+    const handleValueChange = (value: string) => {
+        console.log('%c⧭', 'color: #aa00ff', 'changing the value of variable ', currentVariable, 'to ', value);
+    };
+
+    return {
+        handleImageChange,
+        handleImageRemove,
+        handleValueChange,
+        closePanel,
+    };
+};
