d41888c4fd3fb7fc955f0ca3b2a6ec5d3f1129a6
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2023-05-11 10:03:16 +0300

    put media connector and preview err url in env

--- src/components/variablesComponents/imageVariable/ImageVariable.tsx
+++ src/components/variablesComponents/imageVariable/ImageVariable.tsx
@@ -4,18 +4,21 @@ import { ImagePicker, Label } from '@chili-publish/grafx-shared-components';
 import { usePreviewImages } from './usePreviewImage';
 import { IImageVariable } from '../VariablesComponents.types';
 
-const DEFAULT_MEDIA_CONNECTOR = 'grafx-media';
-const PREVIEW_ERROR_URL = 'https://cdnepgrafxstudioprd.azureedge.net/shared/assets/preview-fallback-padded.svg';
-
 function ImageVariable(props: IImageVariable) {
     const { variable, handleImageRemove } = props;
     const [selectedImage] = useState<Media>();
+
     const previewCall = (id: string): Promise<Uint8Array> =>
-        window.SDK.mediaConnector.download(DEFAULT_MEDIA_CONNECTOR, id, MediaDownloadType.LowResolutionWeb, {});
+        window.SDK.mediaConnector.download(
+            import.meta.env.VITE_DEFAULT_MEDIA_CONNECTOR,
+            id,
+            MediaDownloadType.LowResolutionWeb,
+            {},
+        );
 
     const { currentPreviewImage } = usePreviewImages(
-        DEFAULT_MEDIA_CONNECTOR,
-        PREVIEW_ERROR_URL,
+        import.meta.env.VITE_DEFAULT_MEDIA_CONNECTOR,
+        import.meta.env.VITE_PREVIEW_ERROR_URL,
         true,
         previewCall,
         selectedImage,
@@ -30,7 +33,7 @@ function ImageVariable(props: IImageVariable) {
             // TODO: replace with the action that opens the assets panel
             // eslint-disable-next-line no-console
             onClick={() => console.log('open assets panel')}
-            previewErrorUrl={PREVIEW_ERROR_URL}
+            previewErrorUrl={import.meta.env.VITE_PREVIEW_ERROR_URL}
         />
     );
 }
