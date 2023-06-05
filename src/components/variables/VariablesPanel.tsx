e38a0a5764e0c1dd5ef3badc03bdb357ecd99092
Piotr Gacek <19873798+pkgacek@users.noreply.github.com>
2023-06-05 12:57:17 +0200

    [Fix] styling issues

--- src/components/variables/VariablesPanel.tsx
+++ src/components/variables/VariablesPanel.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { AvailableIcons, Button, ButtonVariant, FontSizes, Icon, Tray } from '@chili-publish/grafx-shared-components';
 import { Variable } from '@chili-publish/studio-sdk';
 import { css } from 'styled-components';
+import { Colors } from '@chili-publish/grafx-shared-components/lib';
 import { EditButtonWrapper } from './VariablesPanel.styles';
 import VariableComponent from '../variablesComponents/VariablesComponents';
 
@@ -23,7 +24,7 @@ function VariablesPanel(props: { variables: Variable[] }) {
                     styles={css`
                         border-radius: 3rem;
                         padding: 0.9375rem;
-                        fontsize: ${FontSizes.button};
+                        fontsize: ${FontSizes.regular};
 
                         svg {
                             width: 1.125rem !important;
@@ -31,8 +32,14 @@ function VariablesPanel(props: { variables: Variable[] }) {
                     `}
                 />
             </EditButtonWrapper>
-            <Tray isOpen={isVariablesPanelVisible} close={closeVariablePanel} title="Customize">
-                <div style={{ marginTop: '30px' }}>
+            <Tray
+                isOpen={isVariablesPanelVisible}
+                close={closeVariablePanel}
+                title={
+                    <h2 style={{ fontSize: '18px', fontWeight: 500, color: Colors.LIGHT_PRIMARY_WHITE }}>Customize</h2>
+                }
+            >
+                <div style={{ marginTop: '8px' }}>
                     {variables.length > 0 &&
                         variables.map((variable: Variable) => {
                             return (
