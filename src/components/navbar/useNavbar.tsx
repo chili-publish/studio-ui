417e8c633599353a377881b962ae023ef2313291
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2023-06-23 10:51:38 +0300

    add comment about zoom value

--- src/components/navbar/useNavbar.tsx
+++ src/components/navbar/useNavbar.tsx
@@ -41,12 +41,14 @@ const useNavbar = (
 
     const zoomIn = useCallback(async () => {
         (async () => {
+            // 1.142 is the same value used in the engine to zoom in
             await window.SDK.canvas.setZoomPercentage(zoom * 1.142);
         })();
     }, [zoom]);
 
     const zoomOut = useCallback(async () => {
         (async () => {
+            // 0.875 is the same value used in the engine to zoom out
             await window.SDK.canvas.setZoomPercentage(zoom * 0.875);
         })();
     }, [zoom]);
