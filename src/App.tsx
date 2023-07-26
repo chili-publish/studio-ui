3ce1c117a03648a69d3ad3ccdd297855b46a3ccb
Abdelhalim Khouas <42040066+abdelhalimkhouas@users.noreply.github.com>
2023-07-26 11:21:55 +0300

    [Fix] authenticate grafx-font (#54)

--- src/App.tsx
+++ src/App.tsx
@@ -231,6 +231,9 @@ function App({ projectConfig, editorLink }: { projectConfig?: ProjectConfig; edi
                     await window.SDK.connector.configure('grafx-media', async (configurator) => {
                         await configurator.setChiliToken(authToken);
                     });
+                    await window.SDK.connector.configure('grafx-font', async (configurator) => {
+                        await configurator.setChiliToken(authToken);
+                    });
                 }
             }
         };
