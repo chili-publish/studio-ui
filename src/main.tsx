e3e2d57a7bef3e9355463b2e3968bcae8732f425
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2023-11-06 16:29:23 +0300

    remove duplicated import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {
-    IStudioUILoaderConfig,
     DownloadLinkResult,
     OutputSettings,
     Project,
