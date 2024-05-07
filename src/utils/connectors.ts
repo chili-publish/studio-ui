6ced467b8927b861d4f1ed48880c1f8ee2134357
Pavel Samusev <psamusev@chili-publish.com>
2024-05-07 10:43:30 +0300

    chore: fixed typo

--- src/utils/connectors.ts
+++ src/utils/connectors.ts
@@ -15,7 +15,7 @@ export function isAuthenticationRequired(connector: MediaRemoteConnector) {
     return connector.supportedAuthentication.browser.includes('oAuth2AuthorizationCode');
 }
 
-// The idea is to make any call to connector and if it's unahtorized corresponding "onAuthExpired" callback is going to be called in App.tsx
+// The idea is to make any call to connector and if it's unathorized corresponding "onAuthExpired" callback is going to be called in App.tsx
 // that runs authentication for the connecotr's authentication type that requires it
 // If connector is authorized or don't required user authentication this request just return results
 export async function verifyAuthentication(connectorId: string) {
