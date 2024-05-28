2209e8ca5d7d71271a1ec71ff00cb64060f12040
Pavel Samusev <psamusev@chili-publish.com>
2024-05-28 17:50:34 +0300

    chore: revert type casting

--- src/components/variablesComponents/useVariablesComponents.ts
+++ src/components/variablesComponents/useVariablesComponents.ts
@@ -23,7 +23,7 @@ export const useVariableComponents = (currentVariableId: Id) => {
     // TODO: add number to value types after SDK numbers PR is merged
     const handleValueChange = async (value: string | boolean) => {
         if (!currentVariableId) return null;
-        return window.SDK.variable.setValue(currentVariableId, value as any);
+        return window.SDK.variable.setValue(currentVariableId, value);
     };
 
     return {
