020c59d7d9794adc0d6a7cb2f306be43a259e18a
Pavel Samusev <psamusev@chili-publish.com>
2024-06-12 14:41:38 +0300

    chore: remove comments

--- src/tests/components/variablesComponents/imageVariable/useMediaDetails.test.ts
+++ src/tests/components/variablesComponents/imageVariable/useMediaDetails.test.ts
@@ -266,43 +266,4 @@ describe('"useMediaDetails" hook', () => {
             });
         });
     });
-
-    // it('should call "download" with "ready" state check', async () => {
-    //     window.SDK.mediaConnector.download = jest.fn().mockRejectedValueOnce('Random Error');
-    //     window.SDK.connector.getState = jest.fn().mockResolvedValueOnce({
-    //         parsedData: {
-    //             type: 'ready',
-    //         },
-    //     });
-    //     const { result } = renderHook(() => usePreviewImageUrl('grafx-media', 'media-asset-id'));
-
-    //     await waitFor(() => expect(window.SDK.connector.getState).toHaveBeenCalledWith('grafx-media'));
-
-    //     act(() => {
-    //         expect(window.SDK.connector.waitToBeReady).not.toHaveBeenCalled();
-    //         expect(window.SDK.mediaConnector.download).toHaveBeenCalledTimes(1);
-    //         expect(result.current).toEqual(null);
-    //     });
-    // });
-
-    // it('should call "download" with "loading" state check', async () => {
-    //     window.SDK.mediaConnector.download = jest
-    //         .fn()
-    //         .mockRejectedValueOnce('Random Error')
-    //         .mockResolvedValueOnce(new Uint8Array());
-    //     window.SDK.connector.getState = jest.fn().mockResolvedValueOnce({
-    //         parsedData: {
-    //             type: 'loading',
-    //         },
-    //     });
-    //     const { result } = renderHook(() => usePreviewImageUrl('grafx-media', 'media-asset-id'));
-
-    //     await waitFor(() => expect(window.SDK.connector.getState).toHaveBeenCalledWith('grafx-media'));
-
-    //     act(() => {
-    //         expect(window.SDK.connector.waitToBeReady).toHaveBeenCalledWith('grafx-media');
-    //         expect(window.SDK.mediaConnector.download).toHaveBeenCalledTimes(2);
-    //         expect(result.current).toEqual('http://url.com');
-    //     });
-    // });
 });
