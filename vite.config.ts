e95911ed92b9efea0da4c2576f89cf0c9736a74b
Alexandra Bri <popalexandra45@gmail.com>
2024-06-18 11:03:47 +0300

    Use iife to format the bundle

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,39 @@ import react from '@vitejs/plugin-react-swc';
 import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), EnvironmentPlugin(['NODE_ENV', 'PREVIEW_ERROR_URL'])],
-    server: {
-        port: 3002,
-    },
-    base: './',
-    optimizeDeps: {
-        include: ['@chili-publish/grafx-shared-components', '@chili-publish/studio-sdk'],
-    },
-    resolve: {
-        // force Vite to always resolve listed dependencies to the same copy (from project root).
-        dedupe: ['react', 'react-dom'],
-    },
-    build: {
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                index: './src/main.tsx',
-                // bootstrap: './src/_dev-execution/bootstrap.ts',
-            },
-            output: {
-                entryFileNames: 'bundle.js',
-                chunkFileNames: 'bundle.js',
-                assetFileNames: 'main.[ext]',
-                format: 'iife',
+export default ({ mode }) => {
+    return defineConfig({
+        plugins: [react(), EnvironmentPlugin(['NODE_ENV', 'PREVIEW_ERROR_URL'])],
+        server: {
+            port: 3002,
+        },
+        base: './',
+        optimizeDeps: {
+            include: ['@chili-publish/grafx-shared-components', '@chili-publish/studio-sdk'],
+        },
+        resolve: {
+            // force Vite to always resolve listed dependencies to the same copy (from project root).
+            dedupe: ['react', 'react-dom'],
+        },
+        build: {
+            emptyOutDir: true,
+            rollupOptions: {
+                input:
+                    mode === 'development'
+                        ? {
+                              index: './src/main.tsx',
+                              bootstrap: './src/_dev-execution/bootstrap.ts',
+                          }
+                        : {
+                              index: './src/main.tsx',
+                          },
+                output: {
+                    entryFileNames: 'bundle.js',
+                    chunkFileNames: 'bundle.js',
+                    assetFileNames: 'main.[ext]',
+                    format: 'iife',
+                },
             },
         },
-    },
-});
+    });
+};
