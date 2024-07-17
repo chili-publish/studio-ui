bc23dda51b638af7e8026f37b1fa0316039fa1ff
alexandrabri <popalexandra45@gmail.com>
2024-07-17 10:23:16 +0300

    [Feature] [WRS-1865] Refresh token tests (#175)

---
+++ __mocks__/mockOutputSetting.ts
@@ -0,0 +1,10 @@
+export const mockOutputSetting = {
+    id: '1',
+    name: 'GIF',
+    type: 'GIF',
+    description: 'description',
+    fps: 30,
+    maxFileSizeInKiloBytes: 120,
+    scaling: 1.5,
+    watermark: true,
+};
