d1dc3b356e023095c3cb89cb9eeb8065c79f67a7
brapoprod <broels@chili-publish.com>
2024-08-06 16:05:02 +0200

    [Fix] Post release cleanup for actions

--- validate_versions.cjs
+++ validate_versions.cjs
@@ -1,13 +1,13 @@
-/* eslint-disable no-console */
 const packageJson = require('./package.json');
 
-const wrsVersion = packageJson.version.split('.');
-const SDKVersion = packageJson.dependencies['@chili-publish/studio-sdk'].replace('^', '').split('.');
+const SDKVersion = packageJson.dependencies['@chili-publish/studio-sdk'].replace('^', '');
+const semverRegex = /^(\d+)\.(\d+)\.(\d+)(?:-([\da-z\-]+(?:\.[\da-z\-]+)*))?(?:\+([\da-z\-]+(?:\.[\da-z\-]+)*))?$/i;
 
-if (wrsVersion[0] !== SDKVersion[0] || wrsVersion[1] !== SDKVersion[1]) {
-    console.log('\x1b[36m%s\x1b[0m', `Studio UI version ==> ${wrsVersion.join('.')}`);
-    console.log('\x1b[36m%s\x1b[0m', `Studio SDK version ==> ${SDKVersion.join('.')}`);
-    console.log('\x1b[1m\x1b[31m%s\x1b[0m', '⚡Please align major and minor version of Studio UI with Studio SDK ⚡');
+if (semverRegex.test(SDKVersion)) {
+    console.log(`${SDKVersion} is a valid semver version.`);
+} else {
+    console.log('\x1b[1m\x1b[31m%s\x1b[0m', `${SDKVersion} is not a valid semver version.`);
+    console.log('\x1b[1m\x1b[31m%s\x1b[0m', '⚡Please use a supported studio-sdk version ⚡');
 
     throw new Error();
 }
