3ae4fec6fff9ea6cfb5465b435548535394c2d73
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2024-09-18 15:52:52 +0300

    remove log

--- src/components/itemBrowser/ItemBrowser.tsx
+++ src/components/itemBrowser/ItemBrowser.tsx
@@ -127,9 +127,6 @@ function ItemBrowser<
                     },
                     {},
                 );
-
-                // eslint-disable-next-line no-console
-                console.log('%c⧭ debug', 'color: #408059', data);
                 // parse the token used if we need to fetch the next page
                 if (data.success && data.parsedData && data.parsedData.data) {
                     if (!ignore) {
