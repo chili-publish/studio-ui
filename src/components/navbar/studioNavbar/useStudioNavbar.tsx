153fcca0b43bdd47673b9ed16fba7cc6a1d83f14
Pavel.Samusev <psamusev@users.noreply.github.com>
2024-12-19 16:05:53 +0300

    [Fix][WRS-2174] Use project name as file name for outputs (#250)

--- src/components/navbar/studioNavbar/useStudioNavbar.tsx
+++ src/components/navbar/studioNavbar/useStudioNavbar.tsx
@@ -1,18 +1,18 @@
-import { useCallback, useMemo } from 'react';
 import { useTheme } from '@chili-publish/grafx-shared-components';
-import useNavbarDownloadBtn from '../navbarItems/useNavbarDownloadBtn';
-import useNavbarUndoRedoItems from '../navbarItems/useNavbarUndoRedo';
-import useNavbarZoom from '../navbarItems/useNavbarZoom';
-import { NavbarItemType } from '../Navbar.types';
-import { NavbarGroup, NavbarText } from '../Navbar.styles';
-import useNavbarMenu from '../navbarItems/useNavbarMenu';
-import useNavbarModeToggle from '../navbarItems/useNavbarModeToggle';
-import useUserInterfaceSelector from '../navbarItems/useUserInterfaceSelector';
+import { useCallback, useMemo } from 'react';
 import { ProjectConfig } from '../../../types/types';
 import { SESSION_USER_INTEFACE_ID_KEY } from '../../../utils/constants';
+import { NavbarGroup, NavbarText } from '../Navbar.styles';
+import { NavbarItemType } from '../Navbar.types';
+import useNavbarDownloadBtn from '../navbarItems/useNavbarDownloadBtn';
+import useNavbarMenu from '../navbarItems/useNavbarMenu';
+import useNavbarModeToggle from '../navbarItems/useNavbarModeToggle';
+import useNavbarUndoRedoItems from '../navbarItems/useNavbarUndoRedo';
+import useNavbarZoom from '../navbarItems/useNavbarZoom';
+import useUserInterfaceSelector from '../navbarItems/useUserInterfaceSelector';
 
 interface INavbar {
-    projectName: string | undefined;
+    projectName: string;
     projectConfig: ProjectConfig;
     zoom: number;
     undoStackState: { canRedo: boolean; canUndo: boolean };
@@ -49,7 +49,7 @@ const useStudioNavbar = ({
             content: (
                 <NavbarGroup>
                     <NavbarText aria-label={`Project: ${projectName}`} mode={mode}>
-                        {decodeURI(projectName || '')}
+                        {decodeURI(projectName)}
                     </NavbarText>
                 </NavbarGroup>
             ),
