bee5b25a30819d53e39298970eea51e263963150
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2025-02-10 14:27:50 +0300

    fix font size

--- src/components/load-document-error/LoadDocumentErrorDialog.styles.tsx
+++ src/components/load-document-error/LoadDocumentErrorDialog.styles.tsx
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 
 export const MessageWrapper = styled.div`
     color: ${({ theme }) => theme.themeColors.secondaryTextColor};
-    font-size: ${FontSizes.small};
+    font-size: ${FontSizes.regular};
     margin: 0 auto;
 `;
