7d7635fea3d255bc2fd51fa3575f53e27355c9cb
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2025-04-11 11:40:34 +0300

    avoid using settimeout

--- src/components/variablesComponents/MultiLineTextVariable.tsx
+++ src/components/variablesComponents/MultiLineTextVariable.tsx
@@ -1,6 +1,6 @@
 import { Input, InputLabel, Label, ValidationTypes } from '@chili-publish/grafx-shared-components';
 import { LongTextVariable, ShortTextVariable } from '@chili-publish/studio-sdk';
-import React, { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
 import { getDataIdForSUI, getDataTestIdForSUI } from '../../utils/dataIds';
 import { HelpTextWrapper } from './VariablesComponents.styles';
 import { ITextVariable } from './VariablesComponents.types';
@@ -9,11 +9,12 @@ import { useUiConfigContext } from '../../contexts/UiConfigContext';
 
 function MultiLineTextVariable(props: ITextVariable) {
     const { variable, validationError, onValueChange } = props;
-
     const { onVariableBlur, onVariableFocus } = useUiConfigContext();
 
     const [variableValue, setVariableValue] = useState((variable as LongTextVariable).value);
     const placeholder = getVariablePlaceholder(variable);
+
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
 
     useEffect(() => {
         setVariableValue((variable as ShortTextVariable).value || (variable as LongTextVariable).value);
@@ -24,22 +25,23 @@ function MultiLineTextVariable(props: ITextVariable) {
     };
 
     const handleLineBreakKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
-        const textarea = e.target as HTMLTextAreaElement;
-        const cursorPosition = textarea.selectionStart;
-        const currentTextValue = textarea.value;
         if (e.key === 'Enter') {
             e.preventDefault();
-
+            const textarea = textareaRef.current;
+            if (!textarea) return;
+            const cursorPosition = textarea.selectionStart;
+            const currentTextValue = textarea.value;
+            // To make the hard line break visually similar to the soft line break instead of going with \n\n
+            // I went with \n\u200B (zero-width space), so it will be visiually the same but still can be targeted and replaced by \n\n
+            // when sending it to the engine
             const lineBreak = e.shiftKey ? '\n' : '\n\u200B';
-
             const newTextValue =
                 currentTextValue.substring(0, cursorPosition) + lineBreak + currentTextValue.substring(cursorPosition);
-
-            setVariableValue(newTextValue);
-            setTimeout(() => {
+            textarea.value = newTextValue;
+            requestAnimationFrame(() => {
                 textarea.selectionStart = cursorPosition + lineBreak.length;
                 textarea.selectionEnd = cursorPosition + lineBreak.length;
-            }, 1);
+            });
         }
     };
 
@@ -47,6 +49,7 @@ function MultiLineTextVariable(props: ITextVariable) {
         <HelpTextWrapper>
             <Input
                 name="multi-line-text"
+                ref={textareaRef}
                 type="textarea"
                 width="100%"
                 value={variableValue.replace(/\n\n/g, '\n\u200B')}
@@ -57,9 +60,11 @@ function MultiLineTextVariable(props: ITextVariable) {
                 onFocus={() => onVariableFocus?.(variable.id)}
                 onBlur={() => {
                     const oldValue = (variable as LongTextVariable).value;
-                    onValueChange(variableValue.replace(/\n\u200B/g, '\n\n'), {
-                        changed: oldValue !== variableValue.replace(/\n\u200B/g, '\n\n'),
-                    });
+                    if (textareaRef.current) {
+                        onValueChange(textareaRef.current.value.replace(/\n\u200B/g, '\n\n'), {
+                            changed: oldValue !== textareaRef.current.value.replace(/\n\u200B/g, '\n\n'),
+                        });
+                    }
                     onVariableBlur?.(variable.id);
                 }}
                 onChange={handleVariableChange}
