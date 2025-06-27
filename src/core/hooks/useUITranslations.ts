77f53fd8562fe31056889557081ef9bea7ec63c1
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2025-06-27 11:29:22 +0300

    remove comment

--- src/core/hooks/useUITranslations.ts
+++ src/core/hooks/useUITranslations.ts
@@ -4,7 +4,6 @@ import { selectUITranslations } from 'src/store/reducers/appConfigReducer';
 export const useUITranslations = () => {
     const uiTranslations = useSelector(selectUITranslations);
 
-    // Helper to get a translation for a given path, e.g. ['formBuilder', 'variables', 'header']
     const getUITranslation = (...path: string[]): string | undefined => {
         return path.reduce((obj, key) => {
             if (obj && typeof obj === 'object' && key in obj) {
