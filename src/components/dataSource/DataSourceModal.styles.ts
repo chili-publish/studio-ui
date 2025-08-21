10c797ab2eee714f6852fc0a7a577328e244e703
alexandrabri <popalexandra45@gmail.com>
2025-08-21 13:32:58 +0300

    [Fix][GRAFX-4901] Fix text truncation on left panel (#401)

--- src/components/dataSource/DataSourceModal.styles.ts
+++ src/components/dataSource/DataSourceModal.styles.ts
@@ -5,6 +5,11 @@ export const ModalStyle = createGlobalStyle`
    #${MODAL_ID} {
         padding-bottom: 0;
     }
+    &[data-id='gsc-modal-header'] {
+    white-space: pre-wrap;
+    overflow-wrap: break-word;
+    word-break: break-word;
+    }
     &[data-id='gsc-modal-body'] {
         overflow: auto;
     }
--- src/components/dataSource/DataSourceModal.tsx
+++ src/components/dataSource/DataSourceModal.tsx
@@ -4,6 +4,7 @@ import { useUITranslations } from '../../core/hooks/useUITranslations';
 import { APP_WRAPPER_ID } from '../../utils/constants';
 import { MODAL_ID, ModalStyle } from './DataSourceModal.styles';
 import DataSourceTable from './DataSourceTable';
+import { useUserInterfaceDetailsContext } from '../navbar/UserInterfaceDetailsContext';
 
 interface TableModalProps {
     isOpen: boolean;
@@ -30,8 +31,12 @@ function DataSourceModal({
     onSelectedRowChanged,
 }: TableModalProps) {
     const { getUITranslation } = useUITranslations();
+    const { formBuilder } = useUserInterfaceDetailsContext();
 
-    const title = getUITranslation(['formBuilder', 'datasource', 'header'], 'Data source');
+    const title = getUITranslation(
+        ['formBuilder', 'datasource', 'header'],
+        formBuilder.datasource?.header ?? 'Data source',
+    );
 
     return (
         <>
