1dd44d59c2162a80b76e0a4b7122f5f830d75104
Pavel Samusev <psamusev@chili-publish.com>
2025-09-22 15:31:44 +0300

    chore: add support for sandbox mode when developing locally

---
+++ src/_dev-execution/template-manager.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export interface TemplateDocument {
+    [key: string]: unknown;
+}
+
+export class TemplateManager {
+    private readonly authToken: string;
+
+    private readonly environmentApiBaseUrl: string;
+
+    private readonly templateId: string;
+
+    constructor(authToken: string, environmentApiBaseUrl: string, templateId: string) {
+        this.authToken = authToken;
+        this.environmentApiBaseUrl = environmentApiBaseUrl;
+        this.templateId = templateId;
+    }
+
+    /**
+     * Fetches template document from the environment API and returns it as JSON string
+     */
+    async getTemplateDocumentAsString(): Promise<TemplateDocument> {
+        const response = await axios.get<TemplateDocument>(
+            `${this.environmentApiBaseUrl}/templates/${this.templateId}/download`,
+            { headers: { Authorization: `Bearer ${this.authToken}` } },
+        );
+        return response.data;
+    }
+}
