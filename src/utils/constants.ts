8b256dee3d75ce03271ac868549cfe186914a96e
Brian Roels <54845569+brapoprod@users.noreply.github.com>
2025-10-22 15:31:47 +0200

    [Fix][GRAFX-5091] Remove reference to old domain (#445)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 export const APP_WRAPPER_ID = 'studio-ui-application';
 export const PREVIEW_FALLBACK = 'https://studio-cdn.chiligrafx.com/shared/assets/preview-fallback-padded.svg';
+export const FOLDER_PREVIEW_THUMBNAIL = 'https://studio-cdn.chiligrafx.com/shared/assets/folder-padded.png';
 export const SESSION_USER_INTEFACE_ID_KEY = 'userInterfaceId';
 
 export const PAGES_CONTAINER_HEIGHT = '7.5rem';
