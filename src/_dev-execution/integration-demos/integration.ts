cdd20e02567ddf48955e62eeb310fb15e7968bc3
Alexandra Bri <popalexandra45@gmail.com>
2026-02-12 16:17:06 +0200

    debugging

--- src/_dev-execution/integration-demos/integration.ts
+++ src/_dev-execution/integration-demos/integration.ts
@@ -5,13 +5,15 @@ import StudioUI from '../../main';
 import { IntegrationTokenManager } from './integration-token-manager';
 
 export const runIntegrationTests = async (baseConfig: IStudioUILoaderConfig) => {
+    // eslint-disable-next-line no-console
+    console.log('running integration tests');
     const tokenManager = new IntegrationTokenManager();
     const projectConfig = window.__PROJECT_CONFIG__ || {};
 
     StudioUI.studioUILoaderConfig({
         ...baseConfig,
         ...projectConfig,
-        selector: (projectConfig as IStudioUILoaderConfig)?.selector || 'studio-ui-container',
+        selector: (projectConfig as IStudioUILoaderConfig)?.selector || 'sui-root',
         authToken: await tokenManager.getAccessToken(),
         refreshTokenAction: () => tokenManager.getAccessToken(),
     } as IStudioUILoaderConfig);
