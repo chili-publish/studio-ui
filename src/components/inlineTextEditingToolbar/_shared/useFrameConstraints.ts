c9cbd7c8215fca4558df090dcbd6131275667378
Alexandra Bri <popalexandra45@gmail.com>
2026-02-26 10:42:04 +0200

    address review comments

--- src/components/inlineTextEditingToolbar/_shared/useFrameConstraints.ts
+++ src/components/inlineTextEditingToolbar/_shared/useFrameConstraints.ts
@@ -1,5 +1,5 @@
-import { FrameConstraints } from '@chili-publish/studio-sdk';
-import { useCallback, useEffect, useState } from 'react';
+import { Frame, FrameConstraints } from '@chili-publish/studio-sdk';
+import { useEffect, useEffectEvent, useState } from 'react';
 import { useAppSelector } from 'src/store';
 import { selectedFrameContent } from 'src/store/reducers/frameReducer';
 
@@ -7,14 +7,14 @@ const useFrameConstraints = () => {
     const selectedFrame = useAppSelector(selectedFrameContent);
     const [constraintsData, setConstraintsData] = useState<FrameConstraints | null>(null);
 
-    const getConstraintsData = useCallback(async () => {
-        if (!selectedFrame?.id) {
+    const getConstraintsData = useEffectEvent(async (frame: Frame | null) => {
+        if (!frame?.id) {
             setConstraintsData(null);
             return;
         }
 
         try {
-            const frameConstraints = await window.StudioUISDK.frame.constraints.get(selectedFrame.id);
+            const frameConstraints = await window.StudioUISDK.frame.constraints.get(frame.id);
             if (frameConstraints?.parsedData) {
                 setConstraintsData(frameConstraints.parsedData);
             }
@@ -23,11 +23,11 @@ const useFrameConstraints = () => {
             console.error('Error getting frame constraints');
             setConstraintsData(null);
         }
-    }, [selectedFrame]);
+    });
 
     useEffect(() => {
-        getConstraintsData();
-    }, [getConstraintsData]);
+        getConstraintsData(selectedFrame);
+    }, [selectedFrame]);
 
     return { frameConstraints: constraintsData };
 };
