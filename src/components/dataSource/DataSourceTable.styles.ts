30487314763fa1414df9744659ce1cf304a3ad7e
Alexandra Bri <popalexandra45@gmail.com>
2026-03-05 11:43:17 +0200

    styles cleanup

--- src/components/dataSource/DataSourceTable.styles.ts
+++ src/components/dataSource/DataSourceTable.styles.ts
@@ -1,32 +1,10 @@
 import { FontSizes } from '@chili-publish/grafx-shared-components';
 import styled from 'styled-components';
 
-export const TableWrapper = styled.div`
-    width: 100%;
-    display: contents;
-    // Although it states for margin-bottom, actually it implies on the right gap between table and right side of modal
-    margin-bottom: 2.5rem;
-    position: relative;
-    table {
-        margin-bottom: 2rem; // 0.5rem takes InfiniteScrollingContainer
-    }
-`;
-
-export const InfiniteScrollingContainer = styled.div`
-    border-top: 1px solid transparent;
-    boder-bottom: 1px solid transparent;
-    // 2px is bottom size
-    margin-bottom: calc(0.5rem - 2px);
-`;
-
-export const Center = styled.div`
+const Center = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-
-    ${TableWrapper} + & {
-        margin-top: -2rem;
-    }
 `;
 
 export const FullSizeCenter = styled(Center)`
