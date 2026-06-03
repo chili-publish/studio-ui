a8ed064ce3800714d9dc1beac8a7fefc9fd04200
Pavel Samusev <psamusev@chili-publish.com>
2026-06-03 14:50:46 +0300

    [Fix][WRS-3056] Fix linking of shared components package
    - Allow vite build with linked packages
    - Allow jest test with linked package
    
    NOTE: There is still a TS compilation issue with linked packages, but this is expected and intentonally not fixed

--- vite.base.config.ts
+++ vite.base.config.ts
@@ -15,15 +15,27 @@ export const baseConfig = (outputFormat?: string) => ({
         port: 3002,
     },
     base: './',
-    optimizeDeps: {
-        // To debug linked version of '@chili-publish/grafx-shared-components', exclude it from this array
-        include: [],
-    },
     resolve: {
-        // force Vite to always resolve listed dependencies to the same copy (from project root).
-        dedupe: ['react', 'react-dom'],
+        dedupe: [
+            'react',
+            'react-dom',
+            'styled-components',
+            'react-dnd',
+            'react-dnd-html5-backend',
+            'react-dnd-touch-backend',
+            'dnd-core',
+            'react-select',
+            'react-datepicker',
+            'date-fns',
+            '@fortawesome/fontawesome-svg-core',
+            '@fortawesome/react-fontawesome',
+            '@fortawesome/pro-light-svg-icons',
+            '@fortawesome/pro-regular-svg-icons',
+            '@fortawesome/pro-solid-svg-icons',
+        ],
         alias: {
             'react/jsx-runtime.js': 'react/jsx-runtime',
+            'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
             src: path.resolve(__dirname, './src'),
         },
     },
